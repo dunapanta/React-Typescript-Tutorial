7093b8285f91179d337d621ec5df75f21f1b89fb
Daniel <dunapanta2@gmail.com>
2021-04-06 21:55:21 -0500

    Desplegando tabla

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -3,15 +3,38 @@ import { reqResApi } from "../api/reqRes";
 import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export default function Usuarios() {
-    const [Usuarios, setUsuarios] = useState<Usuario[]>([])
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
   useEffect(() => {
     reqResApi
       .get<ReqResListado>("/users")
       .then((resp) => {
         console.log(resp.data.data);
+        setUsuarios(resp.data.data);
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const renderItem = (usuario: Usuario) => {
+    const { id, first_name, last_name, email, avatar } = usuario;
+    return (
+      <tr key={id.toString()}>
+        <td>
+          <img
+            src={avatar}
+            alt={first_name}
+            style={{
+              width: 40,
+              borderRadius: 100,
+            }}
+          />
+        </td>
+        <td>
+          {first_name} {last_name}
+        </td>
+        <td>{email}</td>
+      </tr>
+    );
+  };
 
   return (
     <>
@@ -24,8 +47,9 @@ export default function Usuarios() {
             <th>email</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{usuarios.map(renderItem)}</tbody>
       </table>
+      <button className="btn btn-primary">Siguientes</button>
     </>
   );
 }
